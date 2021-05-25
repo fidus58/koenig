8526cef007b671c129c68372fa50d5ff766429dd
fidus58 <ahrens@informatik.hu-berlin.de>
2021-05-25 09:01:39 +0200

    Initial Commit

---
+++ koenig/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  koenig
+//
+//  Created by Klaus Ahrens on 25.05.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
